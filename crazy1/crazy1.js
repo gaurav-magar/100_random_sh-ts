b99d2b129aba56af3c72c974d3fc15798e423d37
Magar-Gaurav <gauravmagar664@gmail.com>
2026-02-09 05:49:12 +0545

    Upgraded css and js code to take input from user

--- crazy1/crazy1.js
+++ crazy1/crazy1.js
@@ -1,26 +1,62 @@
-function spins(){
-    let boxes = document.getElementById("box");
-    let options = ["read","code","make notes","sleep"];
+let boxes = document.getElementById("box");
+let options = [];
+let colors = ["red", "green", "blue", "orange", "purple", "navy", "maroon", "teal", "gray"];
+if (confirm("Do you want to enter your own choices?:")) {
+    let n = prompt("Enter the number of choices you want to enter");
+    for (let i = 0; i < n; i++) {
+        options[i] = prompt(`Enter the ${i + 1} choice: `);
+
+        let newDiv = document.createElement("div");
+        newDiv.textContent = options[i];
+
+        let colorsIndex = Math.floor(Math.random() * colors.length);
+        newDiv.style.backgroundColor = colors[colorsIndex];
+        newDiv.style.color = "white";
+
+        newDiv.classList.add(colors[colorsIndex], "boxes");
+        boxes.appendChild(newDiv);
+    }
+}
+else {
+    options = ["read", "code", "make notes", "sleep"];
+    for(let i =0; i< options.length; i++){
+
+        let newDiv = document.createElement("div");
+        newDiv.textContent = options[i];
+        
+        let colorsIndex = Math.floor(Math.random() * colors.length);
+        newDiv.style.backgroundColor = colors[colorsIndex];
+        newDiv.style.color = "white";
+        
+        newDiv.classList.add(colors[colorsIndex], "boxes");
+        boxes.appendChild(newDiv);
+    }
+}
+function spins() {
     const index = Math.floor(Math.random() * options.length);
 
     let resultBox = document.getElementById("resultContainer");
     resultBox.classList.remove("show");
     resultBox.classList.add("hide");
 
+
     boxes.style.transition = "transform 2s ease-in-out";
     boxes.style.transform = "translateX(1000px)";
 
-    setTimeout(()=>{
+    setTimeout(() => {
         boxes.style.transition = "transform 2s ease-in-out";
         boxes.style.transform = "translateX(-1000px)";
-    },2000);
+    }, 2000);
 
-    setTimeout(()=>{
+    setTimeout(() => {
         boxes.style.transition = "transform 2s ease-in-out";
         boxes.style.transform = "translateX(0)";
+    }, 4000);
+
+    setTimeout(() => {
         document.querySelector(".hide").classList.toggle("show");
         document.getElementById("result").innerHTML = options[index];
         console.log(`index: ${index}, result: ${options[index]}`)
-    },4000);
-    
+
+    }, 6000);
 }
